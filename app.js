c8c8c543f807fe359bf31b9f2fb81736e2f9b1ab
Ahmet Baki Memiş <69093842+mestoness@users.noreply.github.com>
2021-08-12 11:20:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const supabase = createClient(
   "https://*************.supabase.co",
   "*********************"
 );
-app.post("/signup", async (req, res) => {
+app.post("/auth/signup", async (req, res) => {
   supabase
     .from("vue_users")
     .insert([
@@ -38,7 +38,7 @@ app.post("/signup", async (req, res) => {
       res.json(err);
     });
 });
-app.post("/login", async (req, res) => {
+app.post("/auth/login", async (req, res) => {
   supabase
     .from("vue_users")
     .select("*")
@@ -48,25 +48,48 @@ app.post("/login", async (req, res) => {
     .then((response) => {
       if (response.data != null) {
         if (!bcrypt.compareSync(req.body.password, response.data[0].password)) {
-          return res.status(401).json({ login: "false" });
+          return res.status(401).json({
+            login: "false",
+          });
         } else {
-          let token = jwt.sign({ userId: response.data[0].id }, SECRET_KEY);
-          res.status(200).json({ login: "true", token: token });
+          let token = jwt.sign(
+            {
+              userId: response.data[0].id,
+              pass_12: bcrypt.hashSync(
+                response.data[0].password.substr(0, 12),
+                10
+              ),
+            },
+            SECRET_KEY,
+            {
+              expiresIn: "1h",
+            }
+          );
+          res.status(200).json({
+            login: "true",
+            token: token,
+          });
         }
+      } else {
+        res.json({
+          error: "email not",
+        });
       }
     })
     .catch((err) => {
-      res.json(err);
+      res.json({
+        dbError: "email not",
+        error: err,
+      });
     });
 });
-app.get("/info", (req, res) => {
-  let token = req.headers.token;
-  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+app.get("/auth/info", verifyToken, (req, res) => {
+  jwt.verify(req.token, SECRET_KEY, (err, decoded) => {
     if (err) {
       return res.status(401).json({
         token_status: "unauthorized",
       });
-    } else {
+    } else if (decoded.pass_12 && decoded.userId) {
       supabase
         .from("vue_users")
         .select("*")
@@ -74,23 +97,57 @@ app.get("/info", (req, res) => {
           id: decoded.userId,
         })
         .then((response) => {
-          res.json([
-            {
+          if (
+            !bcrypt.compareSync(
+              response.data[0].password.substr(0, 12),
+              decoded.pass_12
+            )
+          ) {
+            return res.status(401).json({
+              login: "false",
+            });
+          } else {
+            res.json({
               error: response.error,
-              data: response.data,
+              data: {
+                email: response.data[0].email,
+                name: response.data[0].name,
+              },
               status: response.status,
               statusText: response.statusText,
-            },
-          ]);
+            });
+          }
         })
         .catch((err) => {
-          res.json(err);
+          res.json({
+            dbError: "id not",
+            error: err,
+          });
         });
+    } else {
+      return res.status(401).json({
+        token_status: "unauthorized",
+      });
     }
   });
 });
+app.get("/auth/logout", (req, res) => {
+  res.json({});
+});
 
-const port = process.env.PORT || 8086;
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, (err) => {
   if (err) return console.log(err);
